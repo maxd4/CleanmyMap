af73882c4529c901c022e5198665d75c2ecb0a02
maxd4 <maxd4@users.noreply.github.com>
2026-09-25 05:21:35 +0200

    Réduire le bundle d'accueil

--- apps/web/src/components/accueil/index.ts
+++ apps/web/src/components/accueil/index.ts
@@ -10,4 +10,3 @@ export { HomeCommunityActivity } from './accueil-community-activity';
 export { HomeCommunityCredibility } from './accueil-community-credibility';
 export { HomeFooter } from './accueil-footer';
 export { OriginCredibility } from './OriginCredibility';
-export { ZoneRepartitionChart } from './ZoneRepartitionChart';
